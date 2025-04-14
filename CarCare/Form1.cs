86c6b2d7b8377feacdd420a6634b536023225586
Maximus019BG <maxralev019@gmail.com>
2025-04-14 23:06:15 +0300

    feat: small login page no logic

--- CarCare/Form1.cs
+++ CarCare/Form1.cs
@@ -6,4 +6,29 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void label1_Click_1(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void textBox1_TextChanged_1(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
 }
