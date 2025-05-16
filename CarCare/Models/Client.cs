a41982cf4fb861568b1a7aaa02aed9145632617c
Maximus019BG <maxralev019@gmail.com>
2025-05-16 12:00:00 +0300

    feat: client model

---
+++ CarCare/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿namespace CarCare
+{
+    public class Client
+    {
+        private string name;
+        private string phone;
+        private string carRegistrationNumber;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public string CarRegistrationNumber
+        {
+            get { return carRegistrationNumber; }
+            set { carRegistrationNumber = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {CarRegistrationNumber}";
+        }
+    }
+}
